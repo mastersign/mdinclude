fb72a5210b43fa416e051934dfcb3e22f49e3549
Tobias Kiertscher <dev@mastersign.de>
2015-12-07 11:33:02 +0100

    added gulp task to convert language.yml to JSON

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/* globals require */
+
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var yaml = require('js-yaml');
+var textTransformation = require('gulp-text-simple');
+
+var yaml2json = textTransformation(function (yamlText) {
+	return yaml.safeLoad(yamlText);
+});
+
+gulp.task('update-languages', function (cb) {
+	return gulp.src('bower_components/languages/index.yml')
+		.pipe(yaml2json())
+		.pipe(rename({ basename: 'languages', extname: '.json' }))
+		.pipe(gulp.dest('res/'))
+});
+
+gulp.task('default', ['update-languages']);
